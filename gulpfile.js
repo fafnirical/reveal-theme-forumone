09c072bd5583eb0ce61c1a4afb51826bf0abad2e
Wade Burelbach <wburelbach@forumone.com>
2017-03-13 14:46:27 -0700

    build(gulp): add build, watch gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,39 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
 
-gulp.task('default', []);
+const paths = {
+  styles: {
+    src: './src/sass',
+    dist: './dist/css',
+  },
+};
+
+// Build styles task
+gulp.task('build:styles', () => (
+  gulp
+    .src(`${paths.styles.src}/**/*.scss`)
+    .pipe(sassGlob())
+    .pipe(
+      sass({
+        sourceMap: true,
+        outFile: `${paths.styles.dist}/forumone.css`,
+        outputStyle: 'expanded',
+        includePaths: [
+          './node_modules',
+        ],
+        quiet: true,
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest(`${paths.styles.dist}`))
+));
+gulp.task('watch:styles', () => (
+  gulp.watch(`${paths.styles.src}/**/*.scss`, ['build:styles'])
+));
+
+// Build task
+gulp.task('build', ['build:styles']);
+gulp.task('watch', ['watch:styles']);
+
+// Default task
+gulp.task('default', ['build']);
